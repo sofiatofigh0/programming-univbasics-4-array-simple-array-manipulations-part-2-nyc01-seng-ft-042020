304fc2eb422a9b6b50f8b20feb9e4597afcf24ac
sofia tofigh <sst2140@columbia.edu>
2020-04-02 05:36:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,18 @@ end
 def using_insert(array,element)
   array.insert(4,element)
 end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+using_delete_at(array,integer)
+array.delete_at(integer)
